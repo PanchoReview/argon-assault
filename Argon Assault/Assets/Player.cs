ab8d0c8b6b347f9c7a4d2636c3823d584744cdd3
Francisco Astorga <fastorga@nimok.com>
2020-05-10 17:26:54 -0400

    Se añade clase Player.cs que permite detectar movimiento de la nave.
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

---
+++ Argon Assault/Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //Obtengo eje horizontal
+        print(horizontalThrow); //Imprimo la inclinación del axis Horizontal, para saber si el player está moviendo a la izquierda o derecha.
+
+    }
+}
